84da251ebd1517b89c07a7cdeeaecf61423ad9de
Richard-Scap <scap222@gmail.com>
2016-08-08 17:27:49 -0500

    convert tabs to spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
-	# metadata for pagination
-	def meta(obj)
-		@meta = {
-			:current_page => obj.current_page,
-			:per_page => obj.per_page,
-			:total_pages => obj.total_pages,
-			:current_entries => obj.to_a.min.id..index.to_a.max.id,
-			:total_entries => obj.total_entries,
-			:entries => obj
-		}
-	end
+  # metadata for pagination
+  def meta(obj)
+    @meta = {
+      :current_page => obj.current_page,
+      :per_page => obj.per_page,
+      :total_pages => obj.total_pages,
+      :current_entries => obj.to_a.min.id..index.to_a.max.id,
+      :total_entries => obj.total_entries,
+      :entries => obj
+    }
+  end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,4 @@
 class Business < ApplicationRecord
-	# validate the uuid of each business is present and unique
-	validates :uuid, presence: true, uniqueness: true
+  # validate the uuid of each business is present and unique
+  validates :uuid, presence: true, uniqueness: true
 end
